c6546e51926a6e66924545cf207aca51037dd63e
jross <jross@isecpartners.com>
2015-01-20 19:37:23 -0500

    updated html, added rules page

--- web/app.js
+++ web/app.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var app = express()
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.disable('x-powered-by');
-
-app.get('/', function(request, response) {
-//   throw 'Error';
-  response.status(200).json('OK');
-});
 
 // include the /users routing
 var users = require('./routes/users');
