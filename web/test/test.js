ec506dcb3b4acf39902bcba94e065903e0ff9159
jross <jross@isecpartners.com>
2015-01-20 20:37:09 -0500

    added reset css. modified test to include /rules.html and (non-existant) api

--- web/test/test.js
+++ web/test/test.js
@@ -14,5 +14,32 @@ describe('Requests to the app root', function() {
       .get('/')
       .expect('Content-Type', /html/, done);
   });
+});
 
+describe('Requests to /rules.html', function() {
+  it('Should return status code 200', function(done) {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+
+  it('Should return HTML format', function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /html/, done);
+  });
+});
+
+describe('Requests to /api/grid', function() {
+  it('Should return status code 200', function(done) {
+    request(app)
+      .get('/api/grid')
+      .expect(200, done);
+  });
+
+  it('Should return JSON format', function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /json/, done);
+  });
 });
